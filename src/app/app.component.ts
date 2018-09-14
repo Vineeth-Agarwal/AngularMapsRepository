e3d855bbf0a07931c15b283c5c0bf3a75839baa7
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-09-14 09:12:16 -0500

    working maps functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  zoom:number=10;
+  lat=40.349841283655195;
+  lng=-94.8827696546299;
+  // Markers
+  markers:marker[]=[
+    {
+      name: 'location1',
+      deceased:0,
+      immediate:2,
+      delayed:3,
+      minor:10,
+      lat:40.349841283655195,
+      lng:-94.8827696546299,
+      draggable:true
+    },
+    {
+      name: 'location2',
+      deceased:0,
+      immediate:2,
+      delayed:3,
+      minor:10,      
+      lat:40.549841283655195,
+      lng:-94.6827696546299,
+      draggable:true
+    },
+    {
+      name: 'location3',
+      deceased:0,
+      immediate:2,
+      delayed:3,
+      minor:10,
+      lat:40.549841283655195,
+      lng:-94.4827696546299,
+      draggable:true
+    },
+
+    {
+      name: 'location4',
+      deceased:0,
+      immediate:2,
+      delayed:3,
+      minor:10,
+      lat:40.249841283655195,
+      lng:-94.9827696546299,
+      draggable:true
+    },
+
+    {
+      name: 'location5',
+      deceased:0,
+      immediate:2,
+      delayed:3,
+      minor:10,
+      lat:40.249841283655195,
+      lng:-94.2827696546299,
+      draggable:true
+    }
+
+
+  ]
+
+constructor(){
+
 }
+}
+// Marker Type
+interface marker{
+  name?:string;
+  deceased:number;
+  immediate:number;
+  delayed:number;
+  minor:number;
+  lat:number;
+  lng:number;
+  draggable:boolean
+}
+
+  // onChoseLocation(event){
+  //   console.log(event);  
+  //   this.latitude=event.coords.lat;
+  //   this.longitude=event.coords.lng;
+  //   // this.latitude2=event.coords.lat;
+  //   // this.longitude2=event.coords.lng;
+
+  // }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import  {AgmCoreModule} from '@agm/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      // apiKey: 'AIzaSyBgW1DJWMWHUVEARVnpkjnLk16iXZ8GAb8'
+      apiKey: 'AIzaSyB954EX24ldvc9K55mjhdei_wg8Ly5shKQ'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
